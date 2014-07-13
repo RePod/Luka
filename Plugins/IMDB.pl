fd436996e48591d495e618cfe4ae3283da4b9ca2
Daniel Caaz <DCaazy@gmail.com>
2014-07-13 15:27:03 -0700

    Minimaler minimal imdb stuff

--- Plugins/IMDB.pl
+++ Plugins/IMDB.pl
@@ -18,8 +18,14 @@ addPlug('IMDB', {
         my $content = get('http://www.omdbapi.com/?i='.${$_}{imdbID});
         $content =~ s/[!\P{IsASCII}]/-/g;
         %imdb = %{decode_json($content)};
-        &{$utility{'Fancify_say'}}($_[0],$_[1],"[http://www.imdb.com/title/$imdb{imdbID}] [\x04$imdb{Title}\x04 ($imdb{Year})] [\x04$imdb{imdbRating}/10\x04] [\x04$imdb{Genre}\x04]") if(!$_[4]);
-        &{$utility{'Fancify_say'}}($_[0],$_[1],"[http://www.imdb.com/title/$imdb{imdbID}] [\x04$imdb{Title}\x04 ($imdb{Year})] [\x04$imdb{Genre}\x04] [\x04$imdb{imdbRating}/10\x04] [$imdb{Runtime}] $imdb{Plot}") if($_[4]);
+        if(!$_[4]) {
+          my @genres = split /, /, $imdb{Genre};
+          foreach(@genres) { $_ = substr($_, 0, 3); }
+          &{$utility{'Fancify_say'}}($_[0],$_[1],"[http://www.imdb.com/title/$imdb{imdbID}] [\x04$imdb{Title}\x04 $imdb{Year}] [\x04$imdb{imdbRating}\x04/\x0410\x04] [\x04".(join "\x04, \x04", @genres)."\x04] [\x04$imdb{Runtime}\x04]");
+        }
+        else {
+          &{$utility{'Fancify_say'}}($_[0],$_[1],"[http://www.imdb.com/title/$imdb{imdbID}] [\x04$imdb{Title}\x04 $imdb{Year}] [\x04$imdb{Genre}\x04] [\x04$imdb{imdbRating}\x04/\x0410\x04] [\x04$imdb{Runtime}\x04] $imdb{Plot}");
+        }
         $i++; if((!$_[3]) || ($i > $_[3])) { last; }
       }
     },
@@ -28,7 +34,7 @@ addPlug('IMDB', {
     '^IMDB (.+)$' => {
       code => sub {
         my $search = $1;
-        if($_[2]{where} =~ /moonspace$/i) { &{$utility{'IMDB_search'}}($_[1]{irc},$_[2]{where},$search,0,0); }
+        if($_[2]{where} =~ /moonspace$|testopia$/i) { &{$utility{'IMDB_search'}}($_[1]{irc},$_[2]{where},$search,0,0); }
         else { &{$utility{'IMDB_search'}}($_[1]{irc},$_[2]{where},$search,0,1); }
       }
     }
