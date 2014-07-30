32c01dd9877a4fb17ce86dfd89737c164e72936d
Daniel Caaz <DCaazy@gmail.com>
2014-07-30 01:45:18 -0700

    LGBT mode added.

--- Plugins/Fancify.pl
+++ Plugins/Fancify.pl
@@ -41,14 +41,48 @@ addPlug("Fancify", {
       elsif($lk{data}{plugins}{'Fancify'}{colors}) { @colors = @{$lk{data}{plugins}{'Fancify'}{colors}}; }
       else { @colors = (14,13); }
       my $color = 0;
+      my $rainbow = 0;
+      my $rbPos = 0;
+      my $rbCode = sub { my $text = '' ; foreach(split //, $_[0]) { $_[1]++; if($_[1] >= @{$_[2]}) { $_[1] = 0; } $text .= "\cC$_[2][$_[1]]".$_; } return $text."\cC$colors[0]"; };
+      my @rb = ('04','07','08','09','11','12','06','13');
+      if(($colors[0] + $colors[1]) == 50) { @colors = (14,14); $rainbow = 1; } 
       my $string = "\cC$colors[0]".$_[0];
       my @string = split //, $string;
-      foreach(@string) { if(/\x04/) { $color++; if($color >= @colors) { $color = 0; } $_ = "\cC$colors[$color]"; } }
+      foreach(@string) {
+        if($rainbowing) { 
+          $rbPos++; if($rbPos >= @rb) { $rbPos = 0; } $_ = "\cC$rb[$rbPos]".$_; 
+        }
+        if(/\x04/) { 
+          $color++; if($color >= @colors) { $color = 0; } 
+          if($rainbow) {
+            if(!$rainbowing) { 
+              $rainbowing = 1;
+              $rbPos = -1;
+              $_ = ''; 
+            }
+            else {
+              $rainbowing = 0;
+              $_ = "\cC$colors[0]"; 
+            }
+          }
+          else {
+            $_ = "\cC$colors[$color]"; 
+          }
+        } 
+      }
       $string = join "", @string;
-      $string =~ s/((?:\#|\@)+[\w\-\/.]+)/\cC$colors[1]$1\cC$colors[0]/g; # Hashtag/Channel coloring
-      $string =~ s/([a-z]+:\/\/\S+\.[a-z]{2,6}\/?(?:[\/\w=?]+)?)/\cC$colors[1]$1\cC$colors[0]/gi; # URL coloring.
-      $string =~ s/(?:\x05|>>)([\w]+)/\cC$colors[1]$1\cC$colors[0]/g; # >> or \x05 word coloring.
-      $string =~ s/\cC\d{1,2}(?:,\d{1,2})?(\cC\d{1,2}(?:,\d{1,2})?)/$1/g; # Remove extra colorcodes.
+      if(!$rainbow) {
+        $string =~ s/((?:\#|\@)+[\w\-\/.]+)/\cC$colors[1]$1\cC$colors[0]/g; # Hashtag/Channel coloring
+        $string =~ s/([a-z]+:\/\/\S+\.[a-z]{2,6}\/?(?:[\/\w=?]+)?)/\cC$colors[1]$1\cC$colors[0]/gi; # URL coloring.
+        $string =~ s/(?:\x05|>>)([\w]+)/\cC$colors[1]$1\cC$colors[0]/g; # >> or \x05 word coloring.
+        $string =~ s/\cC\d{1,2}(?:,\d{1,2})?(\cC\d{1,2}(?:,\d{1,2})?)/$1/g; # Remove extra colorcodes.
+      }
+      else {
+        $string =~ s/((?:\#|\@)+[\w\-\/.]+)/&{$rbCode}($1,\$rbPos,\@rb)/eg; # Hashtag/Channel coloring
+        $string =~ s/([a-z]+:\/\/\S+\.[a-z]{2,6}\/?(?:[\/\w=?]+)?)/&{$rbCode}($1,\$rbPos,\@rb)/egi; # URL coloring.
+        $string =~ s/(?:\x05|>>)([\w]+)/&{$rbCode}($1,\$rbPos,\@rb)/eg; # >> or \x05 word coloring.
+        $string =~ s/\cC\d{1,2}(?:,\d{1,2})?(\cC\d{1,2}(?:,\d{1,2})?)/$1/g; # Remove extra colorcodes.
+      }
       return $string;
     },
     'say' => sub {
